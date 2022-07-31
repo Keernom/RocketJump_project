809ee8788138fa294d3c39f422a4933f63dee4c0
Keernom <keernom.gdev@gmail.com>
2022-07-31 16:20:39 +0300

    GUI scratch with required functional added.

---
+++ Assets/TouchCheck.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TouchCheck : MonoBehaviour
+{
+    int jumpForce = 0;
+    int rotationThisFrame = 50;
+    int rotateDirection = 0;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * rotateDirection * Time.deltaTime);
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * jumpForce * Time.deltaTime);
+    }
+
+    public void Jump(int value)
+    {
+        jumpForce = value;
+        Debug.Log("JUMP");
+    }
+
+    public void Rotate(int rotateValue)
+    {
+        rotateDirection = rotateValue;
+        Debug.Log("ROTATE" + rotateValue);
+    }
+}
