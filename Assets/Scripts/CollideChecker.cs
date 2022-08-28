9e49577f7cf3b1d60759668bd972305f683c67db
Keernom <keernom.gdev@gmail.com>
2022-08-29 02:28:00 +0300

    Jittering during rotation fixed. UI asset pack added. UI rotation buttons and thrust button changed.

---
+++ Assets/Scripts/CollideChecker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollideChecker : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Start"))
+            return;
+
+        if (collision.gameObject.CompareTag("Finish"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            print("BOOM");
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,36 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] int _rotationThisFrame = 50;
+
+    public bool rotating;
+
     int _thrustForce = 0;
-    int _rotationThisFrame = 50;
     int _rotateDirection = 0;
+
+    private Rigidbody rb;
+    private Vector3 eulerAngleVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        eulerAngleVelocity = new Vector3(0, 0, _rotationThisFrame);
+    }
 
     private void Update()
     {
-        transform.Rotate(Vector3.forward * _rotationThisFrame * _rotateDirection * Time.deltaTime);
-        GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * _thrustForce * Time.deltaTime);    
+        if (rotating)
+        {
+            rb.freezeRotation = true;
+            rb.MoveRotation(GetComponent<Rigidbody>().rotation * Quaternion.Euler(_rotateDirection * eulerAngleVelocity * Time.deltaTime));
+            rb.freezeRotation = false;
+        }
+
+
+        rb.AddRelativeForce(Vector3.up * _thrustForce * Time.deltaTime);
     }
 
-    public void Jump(int value)
+    public void Thrust(int value)
     {
         _thrustForce = value;
         Debug.Log("JUMP");
@@ -24,5 +43,10 @@ public class PlayerController : MonoBehaviour
     {
         _rotateDirection = rotateValue;
         Debug.Log("ROTATE" + rotateValue);
+    }
+
+    public void SetRotating(bool value)
+    {
+        rotating = value;
     }
 }
