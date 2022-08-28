5a801405ffd3b37b43e988ce3cbff85d497be0e4
Keernom <keernom.gdev@gmail.com>
2022-08-28 22:51:07 +0300

    TouchCheck script name changed to "PlayerController". Few materials and rocket model added  to simplify prototyping. Start and landing zone added. Scene change after landing added.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    int _thrustForce = 0;
+    int _rotationThisFrame = 50;
+    int _rotateDirection = 0;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotationThisFrame * _rotateDirection * Time.deltaTime);
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * _thrustForce * Time.deltaTime);    
+    }
+
+    public void Jump(int value)
+    {
+        _thrustForce = value;
+        Debug.Log("JUMP");
+    }
+
+    public void Rotate(int rotateValue)
+    {
+        _rotateDirection = rotateValue;
+        Debug.Log("ROTATE" + rotateValue);
+    }
+}
