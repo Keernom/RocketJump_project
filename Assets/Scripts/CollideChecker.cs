37f6f883699b3cdc245bee7569be97c53034ba64
Keernom <keernom.gdev@gmail.com>
2022-08-31 01:18:17 +0300

    Level complete and last level complete effects added. Restart button added. Second level added.

--- Assets/Scripts/CollideChecker.cs
+++ Assets/Scripts/CollideChecker.cs
@@ -4,19 +4,43 @@ using UnityEngine.SceneManagement;
 public class CollideChecker : MonoBehaviour
 {
     [SerializeField] ParticleSystem _explosionEffect;
+    [SerializeField] float _loadDelay = 1f;
+
+    bool _isAlive = true;
+    bool _isLanded = false;
+    int _sceneIndex;
+
+    private void Start()
+    {
+        _sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        print(SceneManager.sceneCountInBuildSettings);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Start"))
             return;
 
-        if (collision.gameObject.CompareTag("Finish"))
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        else
+        if (collision.gameObject.CompareTag("Finish") && _isAlive)
         {
+            if (_sceneIndex == SceneManager.sceneCountInBuildSettings - 1)
+                collision.gameObject.transform.GetChild(0).GetComponent<FireworksLaunch>().FinishLaunch();
+            else
+            {
+                collision.gameObject.transform.GetChild(0).GetComponent<FireworksLaunch>().SuccessLaunch();
+                Invoke("NextLevelLoad", _loadDelay);
+            }
+
+            _isLanded = true;
+        }
+        else if (!_isLanded)
+        {
+            _isAlive = false;
             _explosionEffect.Play();
             GetComponent<PlayerController>().StopThrusting();
             GetComponent<PlayerController>().enabled = false;
         }
     }
+
+    private void NextLevelLoad() => SceneManager.LoadScene(_sceneIndex + 1);
 }
---
+++ Assets/Scripts/FireworksLaunch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FireworksLaunch : MonoBehaviour
+{
+    public void FinishLaunch()
+    {
+        var finishChild = transform.GetChild(0);
+        int childCount = finishChild.childCount;
+        
+        for (int i = 0; i < childCount; i++)
+        {
+            finishChild.GetChild(i).GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+    public void SuccessLaunch()
+    {
+        var finishChild = transform.GetChild(1);
+        finishChild.GetChild(0).GetComponent<ParticleSystem>().Play();
+    }
+}
