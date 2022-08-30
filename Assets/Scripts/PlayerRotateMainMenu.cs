8257b2ce3735eef002d5cda7c7a8dcc8203edba3
Keernom <keernom.gdev@gmail.com>
2022-08-30 15:43:30 +0300

    Clouds asset pack added. Main menu screen changed. Roof in levels added.

---
+++ Assets/Scripts/PlayerRotateMainMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class PlayerRotateMainMenu : MonoBehaviour
+{
+    [SerializeField] float _anglePerSecond = 1f;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up, _anglePerSecond * Time.deltaTime);
+    }
+}
